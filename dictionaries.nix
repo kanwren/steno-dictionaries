09f4081292e3f66fa8af9119dc0417d8eae3e724
Nicole Prindle <nprindle@squareup.com>
2022-10-19 20:17:53 -0400

    Add emily-modifiers and emily-symbols

--- dictionaries.nix
+++ dictionaries.nix
@@ -1,9 +1,14 @@
-{ pkgs }:
+{ pkgs
+, emily-modifiers
+, emily-symbols
+}:
 
 # The order that dictionaries should appear, in order from highest to lowest
 # priority
 [
   ./static-dicts/uni-number-reversals.json
+  "${emily-modifiers}/emily-modifiers.py"
+  "${emily-symbols}/emily-symbols.py"
   ./static-dicts/commands.json
   ./static-dicts/main.json
 ]
--- flake.nix
+++ flake.nix
@@ -2,27 +2,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    emily-modifiers = {
+      url = "github:EPLHREU/emily-modifiers";
+      flake = false;
+    };
+    emily-symbols = {
+      url = "github:EPLHREU/emily-symbols";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.default = self.packages.${system}.steno-dictionaries;
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = self.packages.${system}.steno-dictionaries;
 
-      packages.steno-dictionaries =
-        let
-          dicts = import ./dictionaries.nix { inherit pkgs; };
+        packages.steno-dictionaries =
+          let
+            dicts = import ./dictionaries.nix {
+              inherit pkgs;
+              inherit (inputs) emily-modifiers emily-symbols;
+            };
 
-          inherit (nixpkgs) lib;
-          digits = builtins.stringLength (builtins.toString (builtins.length dicts));
-        in
-        pkgs.runCommand "steno-dictionaries" { } ''
-          mkdir -p $out
-          ${lib.strings.concatImapStringsSep "\n" (index: dict: "cp ${dict} $out/${lib.strings.fixedWidthNumber digits index}_${builtins.baseNameOf dict}") dicts}
-        '';
+            inherit (nixpkgs) lib;
+            digits = builtins.stringLength (builtins.toString (builtins.length dicts));
+          in
+          pkgs.runCommand "steno-dictionaries" { } ''
+            mkdir -p $out
+            ${lib.strings.concatImapStringsSep "\n" (index: dict: "cp ${dict} $out/${lib.strings.fixedWidthNumber digits index}_${builtins.baseNameOf dict}") dicts}
+          '';
 
-      formatter = pkgs.nixpkgs-fmt;
-    });
+        formatter = pkgs.nixpkgs-fmt;
+      });
 }
